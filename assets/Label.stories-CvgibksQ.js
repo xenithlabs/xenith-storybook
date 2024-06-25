10ad627a45236cd00238ced02f5f01849c53182a
hanipcode <hanipcode@users.noreply.github.com>
2024-06-25 19:56:20 +0000

    chore(release): 0.0.62

---
+++ assets/Label.stories-CvgibksQ.js
@@ -0,0 +1,10 @@
+import{j as r}from"./cn-BTg03kWI.js";import{L as s}from"./Label-DIT9I768.js";import{I as m}from"./useClickOutside-By-_3oWr.js";import"./index-l2PZgWEW.js";import"./index-CQG7aoJZ.js";import"./index-q8uFWT42.js";import"./index-CeZp-NtI.js";import"./index-CaNG7YX3.js";import"./index-Bb4qSo10.js";import"./styles-BBItLkJ7.js";import"./Icon-DG9iq8fw.js";import"./search-MFtvWDu4.js";import"./check-4u3gJW-P.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-fvTbNGom.js";import"./floating-ui.dom-BuxmzwbT.js";import"./index-DeznNegx.js";import"./index-D9GVr0Dr.js";import"./Checkbox-C699EMJg.js";import"./index-CZsjDy6p.js";import"./index-Vu1Slila.js";const R={title:"Xenith UI/Components/Label",parameters:{layout:"centered",docs:{subtitle:"Renders an accessible label associated with controls."}},args:{required:!1},tags:["autodocs"]},e={name:"Default",args:{required:!0},render:i=>r.jsxs("div",{className:"grid gap-1",children:[r.jsx(s,{...i,children:"Email"}),r.jsx(m,{type:"email",placeholder:"Email"}),","]})};var a,t,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: "Default",
+  args: {
+    required: true
+  },
+  render: args => <div className="grid gap-1">
+      <Label {...args}>Email</Label>
+      <Input type="email" placeholder="Email" />,
+    </div>
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const _=["DefaultLabel"];export{e as DefaultLabel,_ as __namedExportsOrder,R as default};
