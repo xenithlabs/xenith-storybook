5ff0c6ebc61a32eaa4409a3e14286019c729fcec
hanipcode <hanipcode@users.noreply.github.com>
2024-06-25 22:17:14 +0000

    feat: date support onchange

---
+++ assets/Table.stories-CUyZUQQ8.js
@@ -0,0 +1,67 @@
+import{j as e}from"./cn-BTg03kWI.js";import{T as i,a as c,b as n,c as a,d as H,e as l,f as y,g as A}from"./Table-DZC-lzVy.js";import{B as C}from"./Badge-UOx310Xa.js";import{C as s}from"./Checkbox-CkWw-iSo.js";import"./index-l2PZgWEW.js";import"./sort-down-CKv955Md.js";import"./styles-BBItLkJ7.js";import"./index-Bb4qSo10.js";import"./Text-CzgGQCQf.js";import"./index-q8uFWT42.js";import"./Button-DM_EgQWm.js";import"./styles-DTxftv_w.js";import"./Icon-DG9iq8fw.js";import"./index-CeZp-NtI.js";import"./index-CaNG7YX3.js";import"./index-CZsjDy6p.js";import"./index-Vu1Slila.js";import"./index-DeznNegx.js";import"./check-4u3gJW-P.js";const O={title:"Xenith UI/Components/Table",component:i,tags:["autodocs"],render:()=>e.jsxs(i,{children:[e.jsx(c,{children:e.jsxs(n,{children:[e.jsx(a,{children:e.jsx(s,{})}),e.jsx(a,{className:"min-w-[200px]",children:"Currency"}),e.jsx(a,{className:"min-w-[160px]",children:"Payment Method"}),e.jsx(a,{children:"Payment Channel"}),e.jsx(a,{children:"Status"}),e.jsx(a,{children:"Trx Max Amount"}),e.jsx(a,{children:"Trx Min Amount"}),e.jsx(a,{children:"Fee Type"}),e.jsx(a,{children:"Fee Rate"}),e.jsx(a,{children:"Fee Amount"}),e.jsx(a,{children:"Fee Min Amount"}),e.jsx(a,{sticky:!0,stickyPosition:"right",children:"Fee Max Amount"})]})}),e.jsx(H,{children:e.jsxs(n,{children:[e.jsx(l,{children:e.jsx(s,{})}),e.jsx(l,{children:"Indonesian Rupiah (IDR)"}),e.jsx(l,{children:"Virtual Accounts"}),e.jsx(l,{children:"BCA"}),e.jsx(l,{children:e.jsx(C,{theme:"success",variant:"soft",children:"Active"})}),e.jsx(l,{children:" 1.000.000.000"}),e.jsx(l,{children:"10.000"}),e.jsx(l,{children:"Fixed Rate"}),e.jsx(l,{children:"2%"}),e.jsx(l,{children:"-"}),e.jsx(l,{children:" 1.500"}),e.jsx(l,{sticky:!0,stickyPosition:"right",children:"10.000"})]})})]})},r={},t={render:()=>e.jsxs("div",{children:[e.jsx(i,{children:e.jsx(c,{children:e.jsxs(n,{children:[e.jsx(a,{children:e.jsx(s,{})}),e.jsx(a,{className:"min-w-[200px]",children:"Currency"}),e.jsx(a,{className:"min-w-[160px]",children:"Payment Method"}),e.jsx(a,{children:"Payment Channel"}),e.jsx(a,{children:"Status"}),e.jsx(a,{children:"Trx Max Amount"}),e.jsx(a,{children:"Trx Min Amount"}),e.jsx(a,{children:"Fee Type"})]})})}),e.jsx(y,{title:"No data available",subTitle:"You haven't added any data yet.",hasReset:!0,resetText:"Reset"})]})},d={render:()=>e.jsxs(i,{children:[e.jsx(c,{children:e.jsxs(n,{children:[e.jsx(a,{children:e.jsx(s,{})}),e.jsxs(a,{className:"min-w-[200px]",children:["Currency",e.jsx(A,{})]}),e.jsx(a,{className:"min-w-[160px]",children:"Payment Method"}),e.jsx(a,{children:"Payment Channel"}),e.jsx(a,{children:"Status"}),e.jsx(a,{children:"Trx Max Amount"}),e.jsx(a,{children:"Trx Min Amount"}),e.jsx(a,{children:"Fee Type"}),e.jsx(a,{children:"Fee Rate"}),e.jsx(a,{children:"Fee Amount"}),e.jsx(a,{children:"Fee Min Amount"}),e.jsx(a,{children:"Fee Max Amount"})]})}),e.jsx(H,{children:e.jsxs(n,{children:[e.jsx(l,{children:e.jsx(s,{})}),e.jsx(l,{children:"Indonesian Rupiah (IDR)"}),e.jsx(l,{children:"Virtual Accounts"}),e.jsx(l,{children:"BCA"}),e.jsx(l,{children:e.jsx(C,{theme:"success",variant:"soft",children:"Active"})}),e.jsx(l,{children:" 1.000.000.000"}),e.jsx(l,{children:"10.000"}),e.jsx(l,{children:"Fixed Rate"}),e.jsx(l,{children:"2%"}),e.jsx(l,{children:"-"}),e.jsx(l,{children:" 1.500"}),e.jsx(l,{children:"10.000"})]})})]})};var x,o,T;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:"{}",...(T=(o=r.parameters)==null?void 0:o.docs)==null?void 0:T.source}}};var h,b,m;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: () => <div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>
+              <Checkbox />
+            </TableHead>
+            <TableHead className="min-w-[200px]">Currency</TableHead>
+            <TableHead className="min-w-[160px]">Payment Method</TableHead>
+            <TableHead>Payment Channel</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Trx Max Amount</TableHead>
+            <TableHead>Trx Min Amount</TableHead>
+            <TableHead>Fee Type</TableHead>
+          </TableRow>
+        </TableHeader>
+      </Table>
+      <TableEmptyState title="No data available" subTitle="You haven't added any data yet." hasReset resetText="Reset" />
+    </div>
+}`,...(m=(b=t.parameters)==null?void 0:b.docs)==null?void 0:m.source}}};var j,u,p;d.parameters={...d.parameters,docs:{...(j=d.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>
+            <Checkbox />
+          </TableHead>
+          <TableHead className="min-w-[200px]">
+            Currency
+            <TableHeadAction />
+          </TableHead>
+          <TableHead className="min-w-[160px]">Payment Method</TableHead>
+          <TableHead>Payment Channel</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Trx Max Amount</TableHead>
+          <TableHead>Trx Min Amount</TableHead>
+          <TableHead>Fee Type</TableHead>
+          <TableHead>Fee Rate</TableHead>
+          <TableHead>Fee Amount</TableHead>
+          <TableHead>Fee Min Amount</TableHead>
+          <TableHead>Fee Max Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell>
+            <Checkbox />
+          </TableCell>
+          <TableCell>Indonesian Rupiah (IDR)</TableCell>
+          <TableCell>Virtual Accounts</TableCell>
+          <TableCell>BCA</TableCell>
+          <TableCell>
+            <Badge theme="success" variant="soft">
+              Active
+            </Badge>
+          </TableCell>
+          <TableCell> 1.000.000.000</TableCell>
+          <TableCell>10.000</TableCell>
+          <TableCell>Fixed Rate</TableCell>
+          <TableCell>2%</TableCell>
+          <TableCell>-</TableCell>
+          <TableCell> 1.500</TableCell>
+          <TableCell>10.000</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+}`,...(p=(u=d.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};const U=["Default","WithEmptyState","WithTableHeadAction"];export{r as Default,t as WithEmptyState,d as WithTableHeadAction,U as __namedExportsOrder,O as default};
--- components/DatePicker/DatePicker.d.ts
+++ components/DatePicker/DatePicker.d.ts
@@ -1,2 +1,8 @@
-export declare const DatePicker: () => import("react/jsx-runtime").JSX.Element;
+import * as React from "react";
+interface DatePickerProps extends Omit<React.ComponentPropsWithoutRef<"input">, "onChange"> {
+    date?: Date;
+    onChange: (date?: Date) => void;
+}
+export declare const DatePicker: ({ date: dateProps, onChange, ...inputProps }: DatePickerProps) => import("react/jsx-runtime").JSX.Element;
+export {};
 //# sourceMappingURL=DatePicker.d.ts.map
