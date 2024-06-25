06fbde36f244f12718d2bfbb7ef52480eb1d4926
hanipcode <hanipcode@users.noreply.github.com>
2024-06-25 22:28:37 +0000

    feat: date picker update

--- components/DatePicker/DatePicker.d.ts
+++ components/DatePicker/DatePicker.d.ts
@@ -1,7 +1,7 @@
 import * as React from "react";
 interface DatePickerProps extends Omit<React.ComponentPropsWithoutRef<"input">, "onChange"> {
     date?: Date;
-    onChange: (date?: Date) => void;
+    onChange?: (date?: Date) => void;
 }
 export declare const DatePicker: ({ date: dateProps, onChange, ...inputProps }: DatePickerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
