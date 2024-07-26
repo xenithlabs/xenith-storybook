79c3e9cfeb93451af4b56c8a201fa7e7fbc46803
hanipcode <hanipcode@users.noreply.github.com>
2024-07-26 06:10:31 +0000

    feat: FilterHeader use children instead and compose in product app because need to count with appliedFilter

--- components/Filter/Filter.d.ts
+++ components/Filter/Filter.d.ts
@@ -1,5 +1,5 @@
 import { Sheet } from '../Sheet';
-import { ComponentPropsWithoutRef } from '../../../node_modules/react';
+import { ComponentPropsWithoutRef, PropsWithChildren } from '../../../node_modules/react';
 
 declare const FilterSheet: {
     (props: ComponentPropsWithoutRef<typeof Sheet>): import("react/jsx-runtime").JSX.Element;
@@ -7,8 +7,8 @@ declare const FilterSheet: {
 };
 declare const FilterSheetContent: import('../../../node_modules/react').ForwardRefExoticComponent<import('@/main').SheetContentProps & import('../../../node_modules/react').RefAttributes<HTMLDivElement>>;
 declare const FilterSheetResetAll: () => import("react/jsx-runtime").JSX.Element | null;
-declare const FilterHeader: () => import("react/jsx-runtime").JSX.Element;
+declare const FilterHeader: ({ children }: PropsWithChildren) => import("react/jsx-runtime").JSX.Element;
 declare const FilterFooter: () => import("react/jsx-runtime").JSX.Element;
-declare const FilterTrigger: () => import("react/jsx-runtime").JSX.Element;
+declare const FilterTrigger: import('../../../node_modules/react').ForwardRefExoticComponent<import('@radix-ui/react-dialog').DialogTriggerProps & import('../../../node_modules/react').RefAttributes<HTMLButtonElement>>;
 export { FilterSheet, FilterSheetContent, FilterSheetResetAll, FilterHeader, FilterFooter, FilterTrigger, };
 //# sourceMappingURL=Filter.d.ts.map
