59fb2e0a04410d162cfe4e27418b80f2e321d54f
hanipcode <hanipcode@users.noreply.github.com>
2024-07-29 14:53:00 +0000

    feat: skeletonize (#5)

---
+++ components/Skeleton/Skeletonize.d.ts
@@ -0,0 +1,10 @@
+import { ReactNode } from '../../../node_modules/react';
+
+interface Props {
+    loading: boolean;
+    className: string;
+    children: ReactNode;
+}
+export declare const Skeletonize: ({ loading, className, children }: Props) => string | number | boolean | import("react/jsx-runtime").JSX.Element | Iterable<ReactNode> | null | undefined;
+export {};
+//# sourceMappingURL=Skeletonize.d.ts.map
