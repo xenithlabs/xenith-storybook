435059626212b7f04a4420282a1d0646bf0f7112
hanipcode <hanipcode@users.noreply.github.com>
2024-07-30 04:12:21 +0000

    feat: expanded icon (#6)

--- components/Icon/Icon.d.ts
+++ components/Icon/Icon.d.ts
@@ -10,5 +10,17 @@ interface IconProps extends VariantProps<typeof iconStyles>, HTMLAttributes<HTML
     spin?: boolean;
 }
 export declare const Icon: ({ icon, size, className, spin }: IconProps) => import("react/jsx-runtime").JSX.Element;
+interface IconExpandedProps extends IconProps {
+    closed: ReactNode;
+}
+/**
+ * Icon that can changed by listening to the aria-expanded props
+ * using aria-expanded props because it is a HTML Props that almost
+ * any element have, so that this component can be used inside trigger by
+ * <DropdownTrigger asChild aria-expanded={true}>
+ *  <IconExpanded icon={<OpenedIcon />} closed={<ClosedIcon />} />
+ * </DropdownTrigger>
+ */
+export declare const IconExpanded: ({ closed, icon, ...props }: IconExpandedProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=Icon.d.ts.map
