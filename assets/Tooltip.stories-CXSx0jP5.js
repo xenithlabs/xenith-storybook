3286fb6c4a226f890446b198c4cb77c78323d24d
yesaf <yesaf@users.noreply.github.com>
2024-08-01 08:39:27 +0000

    chore(release): 0.0.138

---
+++ assets/Tooltip.stories-CXSx0jP5.js
@@ -0,0 +1,37 @@
+import{j as e}from"./jsx-runtime-DWbWqHZ-.js";import{T as i,a as s,b as n,c as l,$ as v}from"./Tooltip-iU5alxBT.js";import{F as a}from"./info-BBTJuiSU.js";import{I as d}from"./Icon-ev__gahW.js";import{T as u}from"./Text-UY6J7iD0.js";import"./index-l2PZgWEW.js";import"./index-BgmTQNcy.js";import"./index-CaNG7YX3.js";import"./index-C76mvIvD.js";import"./index-DPMSfDr2.js";import"./index-CfbDgQ9C.js";import"./floating-ui.dom-BuxmzwbT.js";import"./index-BmdjtsoG.js";import"./index-DZOIkxWA.js";import"./index-vMKBQQL5.js";import"./cn-C-5SdYIA.js";import"./index-Bb4qSo10.js";import"./index-Cm6j2cFO.js";import"./styles-BNEhTmYy.js";const X={title:"Xenith UI/Components/Tooltip",component:i,tags:["autodocs"],parameters:{docs:{source:{type:"dynamic"}}},render:()=>e.jsxs("div",{className:"relative inline-flex items-center gap-1",children:["just text",e.jsx(s,{children:e.jsxs(i,{children:[e.jsx(n,{children:e.jsx(d,{icon:e.jsx(a,{})})}),e.jsx(l,{children:e.jsx("p",{children:"Add to library"})})]})})]})},o={},r={render:()=>e.jsx("div",{className:"relative",children:e.jsx(s,{children:e.jsxs(i,{children:[e.jsx(n,{asChild:!0,children:e.jsxs("div",{className:"flex gap-1",children:[e.jsx(u,{children:"sEcond tooltip"}),e.jsx(d,{icon:e.jsx(a,{})})]})}),e.jsx(v,{children:e.jsx(l,{side:"left",sideOffset:40,align:"start",children:e.jsx("p",{children:"Add to library"})})})]})})})},t={render:()=>e.jsx("div",{className:"relative flex justify-center",children:e.jsx(s,{children:e.jsxs(i,{children:[e.jsx(n,{asChild:!0,children:e.jsxs("div",{className:"flex gap-1",children:[e.jsx(u,{children:"sEcond tooltip"}),e.jsx(d,{icon:e.jsx(a,{})})]})}),e.jsx(v,{children:e.jsx(l,{side:"left",sideOffset:500,align:"start",children:e.jsx("p",{children:"Add to library"})})})]})})})};var c,p,m;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var x,f,T;r.parameters={...r.parameters,docs:{...(x=r.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <div className="relative">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="flex gap-1">
+              <Text>sEcond tooltip</Text>
+              <Icon icon={<InfoIcon />} />
+            </div>
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent side="left" sideOffset={40} align="start">
+              <p>Add to library</p>
+            </TooltipContent>
+          </TooltipPortal>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+}`,...(T=(f=r.parameters)==null?void 0:f.docs)==null?void 0:T.source}}};var j,h,g;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <div className="relative flex justify-center">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <div className="flex gap-1">
+              <Text>sEcond tooltip</Text>
+              <Icon icon={<InfoIcon />} />
+            </div>
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent side="left" sideOffset={500} align="start">
+              <p>Add to library</p>
+            </TooltipContent>
+          </TooltipPortal>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+}`,...(g=(h=t.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const k=["Default","NewStory","WithLeftOffset"];export{o as Default,r as NewStory,t as WithLeftOffset,k as __namedExportsOrder,X as default};
