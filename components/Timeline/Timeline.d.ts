6eee1b0dc5d95a9a471d6a814252485da18b44fb
yesaf <yesaf@users.noreply.github.com>
2024-08-05 05:18:35 +0000

    chore: timeline components more composite (#14)

--- components/Timeline/Timeline.d.ts
+++ components/Timeline/Timeline.d.ts
@@ -2,11 +2,16 @@ import { default as React } from '../../../node_modules/react';
 
 declare const Timeline: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & React.RefAttributes<HTMLDivElement>>;
 interface TimelineItemProps extends React.HTMLAttributes<HTMLDivElement> {
-    date: string;
-    dateFormat?: string;
     pointClassName?: string;
-    eventTitle: React.ReactNode | string;
 }
 declare const TimelineItem: React.ForwardRefExoticComponent<TimelineItemProps & React.RefAttributes<HTMLDivElement>>;
-export { Timeline, TimelineItem };
+declare const TimelineHeader: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & React.RefAttributes<HTMLDivElement>>;
+declare const TimelineTitle: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & React.RefAttributes<HTMLDivElement>>;
+interface TimelineDateProps extends React.HTMLAttributes<HTMLDivElement> {
+    date: string | Date;
+    dateFormat?: string;
+}
+declare const TimelineDate: React.ForwardRefExoticComponent<TimelineDateProps & React.RefAttributes<HTMLDivElement>>;
+declare const TimelineContent: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & React.RefAttributes<HTMLDivElement>>;
+export { Timeline, TimelineItem, TimelineHeader, TimelineTitle, TimelineDate, TimelineContent, };
 //# sourceMappingURL=Timeline.d.ts.map
