6f718fa4b21a1876b6b013dec9732a7067920237
hanipcode <hanipcode@users.noreply.github.com>
2024-08-05 02:15:12 +0000

    chore(release): 0.0.142

--- components/Filter/Filter.d.ts
+++ components/Filter/Filter.d.ts
@@ -1,5 +1,5 @@
 import { Sheet } from '../Sheet';
-import { ComponentPropsWithoutRef, PropsWithChildren } from '../../../node_modules/react';
+import { ComponentPropsWithoutRef, HTMLAttributes } from '../../../node_modules/react';
 
 declare const FilterSheet: {
     (props: ComponentPropsWithoutRef<typeof Sheet>): import("react/jsx-runtime").JSX.Element;
@@ -7,7 +7,7 @@ declare const FilterSheet: {
 };
 declare const FilterSheetContent: import('../../../node_modules/react').ForwardRefExoticComponent<import('@/main').SheetContentProps & import('../../../node_modules/react').RefAttributes<HTMLDivElement>>;
 declare const FilterSheetResetAll: () => import("react/jsx-runtime").JSX.Element;
-declare const FilterHeader: ({ children }: PropsWithChildren) => import("react/jsx-runtime").JSX.Element;
+declare const FilterHeader: ({ children, className, }: HTMLAttributes<HTMLDivElement>) => import("react/jsx-runtime").JSX.Element;
 declare const FilterFooter: () => import("react/jsx-runtime").JSX.Element;
 declare const FilterTrigger: import('../../../node_modules/react').ForwardRefExoticComponent<import('@radix-ui/react-dialog').DialogTriggerProps & import('../../../node_modules/react').RefAttributes<HTMLButtonElement>>;
 export { FilterSheet, FilterSheetContent, FilterSheetResetAll, FilterHeader, FilterFooter, FilterTrigger, };
