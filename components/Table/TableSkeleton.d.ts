93a542aeaff3f292a98cd92f97c2fa94b67f1adc
yesaf <yesaf@users.noreply.github.com>
2024-08-06 07:23:44 +0000

    feat: add table skeleton component (#19)

---
+++ components/Table/TableSkeleton.d.ts
@@ -0,0 +1,17 @@
+import { default as React } from '../../../node_modules/react';
+import { TableRow, TableBody, TableCell } from './Table';
+
+interface TableSkeletonCellProps extends React.HTMLAttributes<HTMLDivElement> {
+    tableCellProps?: React.ComponentProps<typeof TableCell>;
+}
+declare const TableSkeletonCell: React.ForwardRefExoticComponent<TableSkeletonCellProps & React.RefAttributes<HTMLTableCellElement>>;
+interface TableSkeletonProps extends React.ComponentProps<typeof TableBody> {
+    rows?: number;
+    columns?: number;
+    skeletonWidth?: string | string[] | readonly string[];
+    skeletonHeight?: string | string[] | readonly string[];
+    tableRowProps?: React.ComponentProps<typeof TableRow>;
+}
+declare const TableSkeleton: React.ForwardRefExoticComponent<Omit<TableSkeletonProps, "ref"> & React.RefAttributes<HTMLTableSectionElement>>;
+export { TableSkeletonCell, TableSkeleton };
+//# sourceMappingURL=TableSkeleton.d.ts.map
