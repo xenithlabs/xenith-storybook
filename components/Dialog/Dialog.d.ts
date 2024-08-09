39f56a7a427a190811c51155bb0ddf217dc37f2d
yesaf <yesaf@users.noreply.github.com>
2024-08-09 08:16:12 +0000

    feat: dialog header container class (#22)

--- components/Dialog/Dialog.d.ts
+++ components/Dialog/Dialog.d.ts
@@ -6,8 +6,11 @@ declare const DialogPortal: React.FC<DialogPrimitive.DialogPortalProps>;
 declare const DialogClose: React.ForwardRefExoticComponent<DialogPrimitive.DialogCloseProps & React.RefAttributes<HTMLButtonElement>>;
 declare const DialogOverlay: React.ForwardRefExoticComponent<Omit<DialogPrimitive.DialogOverlayProps & React.RefAttributes<HTMLDivElement>, "ref"> & React.RefAttributes<HTMLDivElement>>;
 declare const DialogContent: React.ForwardRefExoticComponent<Omit<DialogPrimitive.DialogContentProps & React.RefAttributes<HTMLDivElement>, "ref"> & React.RefAttributes<HTMLDivElement>>;
+interface DialogHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
+    containerClassName?: string;
+}
 declare const DialogHeader: {
-    ({ className, children, ...props }: React.HTMLAttributes<HTMLDivElement>): import("react/jsx-runtime").JSX.Element;
+    ({ className, containerClassName, children, ...props }: DialogHeaderProps): import("react/jsx-runtime").JSX.Element;
     displayName: string;
 };
 declare const DialogFooter: {
