adb48821756774d93e2d995115cf9dff8ecae707
yesaf <yesaf@users.noreply.github.com>
2024-09-26 09:11:54 +0000

    feat: file upload improvements (#35)

--- components/FileUploader/FileUploader.d.ts
+++ components/FileUploader/FileUploader.d.ts
@@ -9,8 +9,11 @@ interface FileUploaderProps extends React.HTMLAttributes<HTMLDivElement>, Varian
 }
 declare const FileUploader: React.ForwardRefExoticComponent<FileUploaderProps & React.RefAttributes<HTMLDivElement>>;
 interface FileUploaderInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    error?: boolean;
+    subTitle?: string;
+    onFilesAdded: (files: File[]) => void;
 }
-export declare const FileUploaderInput: React.ForwardRefExoticComponent<FileUploaderInputProps & React.RefAttributes<HTMLInputElement>>;
+declare const FileUploaderInput: React.ForwardRefExoticComponent<FileUploaderInputProps & React.RefAttributes<HTMLInputElement>>;
 declare const AttachmentList: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & React.RefAttributes<HTMLDivElement>>;
 declare const AttachmentItem: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & React.RefAttributes<HTMLDivElement>>;
 declare const AttachmentImage: React.ForwardRefExoticComponent<React.ImgHTMLAttributes<HTMLImageElement> & React.RefAttributes<HTMLImageElement>>;
@@ -20,5 +23,5 @@ interface AttachmentNameProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 declare const AttachmentName: React.ForwardRefExoticComponent<AttachmentNameProps & React.RefAttributes<HTMLDivElement>>;
 declare const AttachmentActions: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & React.RefAttributes<HTMLDivElement>>;
-export { FileUploader, AttachmentList, AttachmentItem, AttachmentImage, AttachmentInner, AttachmentName, AttachmentActions, };
+export { FileUploader, FileUploaderInput, AttachmentList, AttachmentItem, AttachmentImage, AttachmentInner, AttachmentName, AttachmentActions, };
 //# sourceMappingURL=FileUploader.d.ts.map
