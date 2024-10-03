603d62f0868bbb617500928086013ed9817e74fe
yesaf <yesaf@users.noreply.github.com>
2024-10-03 06:44:35 +0000

    feat: add download icon

--- assets/icons/index.d.ts
+++ assets/icons/index.d.ts
@@ -48,4 +48,5 @@ export { default as PercentCircleIcon } from './percent-circle.svg';
 export { default as LightningIcon } from './lightning.svg';
 export { default as CheckCircleIcon } from './check-circle.svg';
 export { default as BackIcon } from './back.svg';
+export { default as DownloadIcon } from './download.svg';
 //# sourceMappingURL=index.d.ts.map
