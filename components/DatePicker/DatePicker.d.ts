50e32fae46f0b85ec0e5115b794455705192e2e2
hanipcode <hanipcode@users.noreply.github.com>
2024-10-07 05:50:48 +0000

    feat: add from date props (#36)

--- components/DatePicker/DatePicker.d.ts
+++ components/DatePicker/DatePicker.d.ts
@@ -2,7 +2,9 @@ import * as React from "react";
 interface DatePickerProps extends Omit<React.ComponentPropsWithoutRef<"input">, "onChange"> {
     date?: Date;
     onChange?: (date?: Date) => void;
+    fromDate?: Date;
+    toDate?: Date;
 }
-export declare const DatePicker: ({ date: dateProps, onChange, ...inputProps }: DatePickerProps) => import("react/jsx-runtime").JSX.Element;
+export declare const DatePicker: ({ date: dateProps, fromDate, toDate, onChange, ...inputProps }: DatePickerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=DatePicker.d.ts.map
