9271793726582a523fe8dc4a55eaad09728fe250
yesaf <yesaf@users.noreply.github.com>
2024-10-21 14:45:26 +0000

    feat: add error border to date picker

--- components/DatePicker/DatePicker.d.ts
+++ components/DatePicker/DatePicker.d.ts
@@ -4,7 +4,8 @@ interface DatePickerProps extends Omit<React.ComponentPropsWithoutRef<"input">, 
     onChange?: (date?: Date) => void;
     fromDate?: Date;
     toDate?: Date;
+    error?: boolean;
 }
-export declare const DatePicker: ({ date: dateProps, fromDate, toDate, onChange, ...inputProps }: DatePickerProps) => import("react/jsx-runtime").JSX.Element;
+export declare const DatePicker: ({ date: dateProps, fromDate, toDate, onChange, error, ...inputProps }: DatePickerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=DatePicker.d.ts.map
