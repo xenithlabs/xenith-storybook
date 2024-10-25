bce82518ded6325b0d416c67f90b0ed2af536b37
hanipcode <hanipcode@users.noreply.github.com>
2024-10-25 02:17:05 +0000

    chore(release): 0.0.231

--- components/OtpInput/OtpInput.d.ts
+++ components/OtpInput/OtpInput.d.ts
@@ -1,9 +1,10 @@
 interface Props {
     onComplete: (values: string) => void;
+    onClear?: () => void;
     errorMessage?: string;
     className?: string;
     disabled?: boolean;
 }
-export declare const OtpInput: ({ onComplete, errorMessage, className, disabled, }: Props) => import("react/jsx-runtime").JSX.Element;
+export declare const OtpInput: ({ onComplete, errorMessage, onClear, className, disabled, }: Props) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=OtpInput.d.ts.map
