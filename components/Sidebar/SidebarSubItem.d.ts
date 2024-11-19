ae0abae572da3797c1ebd086c7be3f8acff9c533
yesaf <yesaf@users.noreply.github.com>
2024-11-19 09:29:17 +0000

    feat: add disabled & class prop to sidebar sub item

--- components/Sidebar/SidebarSubItem.d.ts
+++ components/Sidebar/SidebarSubItem.d.ts
@@ -1,7 +1,9 @@
 interface SidebarSubItemProps {
     text: string;
     active?: boolean;
+    disabled?: boolean;
+    className?: string;
 }
-declare const SidebarSubItem: ({ text, active }: SidebarSubItemProps) => import("react/jsx-runtime").JSX.Element;
+declare const SidebarSubItem: ({ text, active, disabled, className, }: SidebarSubItemProps) => import("react/jsx-runtime").JSX.Element;
 export { SidebarSubItem };
 //# sourceMappingURL=SidebarSubItem.d.ts.map
