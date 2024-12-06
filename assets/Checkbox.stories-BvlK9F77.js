9b592fb3b5469e47836c3982df3bae61427b0fca
yesaf <yesaf@users.noreply.github.com>
2024-12-06 09:24:41 +0000

    feat: update react day picker (#41)
    * feat: update day picker progress
    
    * fix: classnames and add new calendar story
    
    * chore: remove packageManager

---
+++ assets/Checkbox.stories-BvlK9F77.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-BhBAkL08.js";import{C as o}from"./Checkbox-CEELigyu.js";import{L as m}from"./index-D3x6Zd7j.js";import"./index-caxmlYbZ.js";import"./index-d7ziAqdJ.js";import"./index-DIE_uK0V.js";import"./index-BnmSys5K.js";import"./index-BPrjlVTu.js";import"./index-D1_gb_Tp.js";import"./cn-C-5SdYIA.js";import"./Icon-Bh8OjlaW.js";import"./index-Bb4qSo10.js";import"./check-CDvVW5im.js";import"./index-D09aQd82.js";const D={title:"Xenith UI/Components/Checkbox",component:o,tags:["autodocs"]},r={render:()=>e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(o,{indeterminate:!0}),e.jsx(m,{children:"Agree to terms and condition"})]})},t={render:()=>e.jsxs("div",{className:"flex items-center gap-2",children:[e.jsx(o,{disabled:!0,checked:!0}),e.jsx(m,{children:"Agree to terms and condition"})]})};var s,a,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <div className="flex items-center gap-2">
+      <Checkbox indeterminate />
+      <Label>Agree to terms and condition</Label>
+    </div>
+}`,...(i=(a=r.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var n,c,d;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => <div className="flex items-center gap-2">
+      <Checkbox disabled checked />
+      <Label>Agree to terms and condition</Label>
+    </div>
+}`,...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const N=["Default","DisabledChecked"];export{r as Default,t as DisabledChecked,N as __namedExportsOrder,D as default};
---
+++ assets/Skeleton.stories-6pyUn2nq.js
@@ -0,0 +1,5 @@
+import{j as t}from"./jsx-runtime-BhBAkL08.js";import{S as a}from"./Skeleton-DegGPell.js";import"./index-caxmlYbZ.js";import"./cn-C-5SdYIA.js";const d={title:"Xenith UI/Components/Skeleton",component:a,tags:["autodocs"]},e={render:()=>t.jsx("div",{children:t.jsx(a,{className:"w-[300px] h-[100px]"})})};var o,r,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => <div>
+      <Skeleton className="w-[300px] h-[100px]" />
+    </div>
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,d as default};
