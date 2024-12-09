80848d67084d81cf4262d88ed1b0adaffffcb11c
hanipcode <hanipcode@users.noreply.github.com>
2024-12-09 09:38:14 +0000

    chore(release): 0.0.264

---
+++ assets/Calendar.stories-BwJaIU-A.js
@@ -0,0 +1,9 @@
+import{C as l}from"./Calendar-DsYWLAuM.js";import{t as u,c as D}from"./constructFrom-rJN6zrQ_.js";import"./jsx-runtime-BhBAkL08.js";import"./index-caxmlYbZ.js";import"./cn-C-5SdYIA.js";import"./chevron-left-BzZ3Cg5H.js";import"./chevron-right-C3e6g37q.js";import"./styles-BNEhTmYy.js";import"./index-Bb4qSo10.js";import"./Button-Dpzn5L7J.js";import"./index-D09aQd82.js";import"./spinner-C2p9_Bgj.js";import"./chevron-down-mN_p2z2a.js";import"./Icon-Bh8OjlaW.js";function a(s,o){const e=u(s);return isNaN(o)?D(s,NaN):(o&&e.setDate(e.getDate()+o),e)}const F={title:"Xenith UI/Components/Calendar",component:l,args:{selected:a(new Date,2),mode:"single",required:!1,onSelect:console.log}},r={},t={args:{mode:"range",selected:{from:a(new Date,2),to:a(new Date,5)}}};var n,m,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,i,p;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    mode: "range",
+    selected: {
+      from: addDays(new Date(), 2),
+      to: addDays(new Date(), 5)
+    }
+  }
+}`,...(p=(i=t.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};const I=["Default","RangeMode"];export{r as Default,t as RangeMode,I as __namedExportsOrder,F as default};
