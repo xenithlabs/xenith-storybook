4b6b0360da2b01d64b100821aed1e4c9b1b365e3
yesaf <yesaf@users.noreply.github.com>
2025-01-03 07:25:06 +0000

    chore(release): 0.0.289

--- assets/Calendar.stories-IidBz7Ju.js
+++
@@ -1,9 +0,0 @@
-import{C as d}from"./Calendar-CoT-vS5y.js";import{a as o}from"./addDays-DUX3p28w.js";import"./jsx-runtime-BhBAkL08.js";import"./index-caxmlYbZ.js";import"./cn-C-5SdYIA.js";import"./chevron-left-BzZ3Cg5H.js";import"./chevron-right-C3e6g37q.js";import"./styles-BNEhTmYy.js";import"./index-Bb4qSo10.js";import"./Button-JbOjmgRu.js";import"./index-D09aQd82.js";import"./spinner-C2p9_Bgj.js";import"./chevron-down-mN_p2z2a.js";import"./Icon-7BdkfswG.js";import"./constructFrom-rJN6zrQ_.js";const h={title:"Xenith UI/Components/Calendar",component:d,args:{selected:o(new Date,2),mode:"single",required:!1,onSelect:console.log}},e={},r={args:{mode:"range",selected:{from:o(new Date,2),to:o(new Date,5)}}};var a,t,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var m,n,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  args: {
-    mode: "range",
-    selected: {
-      from: addDays(new Date(), 2),
-      to: addDays(new Date(), 5)
-    }
-  }
-}`,...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const q=["Default","RangeMode"];export{e as Default,r as RangeMode,q as __namedExportsOrder,h as default};
