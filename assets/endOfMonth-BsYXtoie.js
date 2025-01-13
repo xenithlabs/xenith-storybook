381cc543551f8dffb996d495e864b77367264bd7
yesaf <yesaf@users.noreply.github.com>
2025-01-13 06:54:25 +0000

    fix: custom date range

---
+++ assets/endOfMonth-BsYXtoie.js
@@ -0,0 +1,1 @@
+import{t as n}from"./constructFrom-rJN6zrQ_.js";function s(e){const t=n(e);return t.setHours(23,59,59,999),t}function a(e){const t=n(e),o=t.getMonth();return t.setFullYear(t.getFullYear(),o+1,0),t.setHours(23,59,59,999),t}export{s as a,a as e};
--- assets/endOfMonth-CoG5-XNs.js
+++
@@ -1,1 +0,0 @@
-import{t as n}from"./constructFrom-rJN6zrQ_.js";function s(e){const t=n(e),o=t.getMonth();return t.setFullYear(t.getFullYear(),o+1,0),t.setHours(23,59,59,999),t}export{s as e};
