a12d5f1621a8a07f8b608bd8479c481b05cec683
yesaf <yesaf@users.noreply.github.com>
2025-01-20 07:11:06 +0000

    feat: add remove save button prop

--- components/DateTimePicker/DateTimePicker.d.ts
+++ components/DateTimePicker/DateTimePicker.d.ts
@@ -9,7 +9,8 @@ interface DateTimePickerProps extends Omit<React.ComponentPropsWithoutRef<"input
     timeError?: boolean;
     timeMessage?: React.ReactNode;
     popoverProps?: React.ComponentProps<typeof PopoverContent>;
+    withoutSaveButton?: boolean;
 }
-export declare const DateTimePicker: ({ date: dateProps, fromDate, toDate, onChange, error, timeError, timeMessage, popoverProps, ...inputProps }: DateTimePickerProps) => import("react/jsx-runtime").JSX.Element;
+export declare const DateTimePicker: ({ date: dateProps, fromDate, toDate, onChange, error, timeError, timeMessage, withoutSaveButton, popoverProps, ...inputProps }: DateTimePickerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=DateTimePicker.d.ts.map
