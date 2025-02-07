1591b488b54d5e0145096d82fde9da248c5e5ee6
yesaf <yesaf@users.noreply.github.com>
2025-02-07 07:15:31 +0000

    feat: add select all button option

--- components/Select/interface.d.ts
+++ components/Select/interface.d.ts
@@ -24,6 +24,7 @@ export interface SelectProps extends ReactSelectProps, BaseStylesProps {
     float?: boolean;
     searchValue?: string;
     setSearchValue?: Dispatch<SetStateAction<string>>;
+    withSelectAll?: boolean;
 }
 export interface OptionType {
     label: string;
