32554e881ba6896de77b7314d5d122b36c70245c
hanipcode <hanipcode@users.noreply.github.com>
2025-02-10 04:27:30 +0000

    chore(release): 0.0.325

--- components/CircularProgressBar/CircularProgressBar.d.ts
+++ components/CircularProgressBar/CircularProgressBar.d.ts
@@ -2,12 +2,14 @@ interface CircularProgressBarProps {
     max?: number;
     value: number;
     min?: number;
+    width?: number;
+    height?: number;
     radius?: number;
     gaugePrimaryColor?: string;
     gaugeSecondaryColor?: string;
     className?: string;
     strokeWidth?: number;
 }
-export declare function CircularProgressBar({ max, min, value, radius, gaugePrimaryColor, gaugeSecondaryColor, strokeWidth, className, }: CircularProgressBarProps): import("react/jsx-runtime").JSX.Element;
+export declare function CircularProgressBar({ max, min, value, width, height, radius, gaugePrimaryColor, gaugeSecondaryColor, strokeWidth, className, }: CircularProgressBarProps): import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=CircularProgressBar.d.ts.map
