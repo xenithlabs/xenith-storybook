172aa03d5dabe8a341becb41f25281bafa85f0a7
hanipcode <hanipcode@users.noreply.github.com>
2025-02-14 01:38:32 +0000

    chore(release): 0.0.339

--- components/Truncate/Truncate.d.ts
+++ components/Truncate/Truncate.d.ts
@@ -3,10 +3,11 @@ import { ReactNode } from '../../../node_modules/react';
 interface TruncateProps {
     children: ReactNode;
     width?: number;
+    maxWidth?: number | string;
     withTooltip?: boolean;
     withCopy?: boolean;
     fluid?: boolean;
 }
-export declare const Truncate: ({ children, width, withTooltip, withCopy, fluid, }: TruncateProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Truncate: ({ children, width, withTooltip, withCopy, fluid, maxWidth, }: TruncateProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=Truncate.d.ts.map
