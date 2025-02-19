573d918d29ada95936beb18c6e96c3e327180bf5
hanipcode <hanipcode@users.noreply.github.com>
2025-02-19 04:34:10 +0000

    chore(release): 0.0.345

--- tailwindTheme.d.ts
+++ tailwindTheme.d.ts
@@ -6,6 +6,7 @@ export declare const theme: {
         };
         boxShadow: {
             sidebar: string;
+            navbar: string;
             container: string;
             input: string;
             dropdown: string;
