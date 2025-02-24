6afc4c1549ac663247f97aad254aff8c5b9edc3f
hanipcode <hanipcode@users.noreply.github.com>
2025-02-24 06:27:58 +0000

    feat: add support for system color scheme (#54)
    * feat: add support for system color scheme
    
    * feat: add dark mode toggle
    
    * fix: remove react import
    
    * feat: ignore eslint in stsories
    
    * feat: add palette icon
    
    * fix: icon colors

---
+++ assets/ToggleGroup.stories-DCIN2DXm.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{T as l,a as o}from"./ToggleGroup-CkL-Uy7I.js";import"./index-DRjF_FHU.js";import"./index-CxRLcT2J.js";import"./cn-C-5SdYIA.js";import"./index-Bb4qSo10.js";const d={title:"Xenith UI/Components/ToggleGroup",component:l,tags:["autodocs"],args:{type:"single",variant:"outline",defaultValue:"a"}},r={render:()=>e.jsx("div",{className:"flex items-center gap-2",children:e.jsxs(l,{defaultValue:"a",type:"single",variant:"rounded",size:"sm",children:[e.jsx(o,{value:"a",children:"A"}),e.jsx(o,{value:"b",children:"B"}),e.jsx(o,{value:"c",children:"C"})]})})};var a,t,s;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <div className="flex items-center gap-2">
+      <ToggleGroup defaultValue="a" type="single" variant="rounded" size="sm">
+        <ToggleGroupItem value="a">A</ToggleGroupItem>
+        <ToggleGroupItem value="b">B</ToggleGroupItem>
+        <ToggleGroupItem value="c">C</ToggleGroupItem>
+      </ToggleGroup>
+    </div>
+}`,...(s=(t=r.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const c=["Default"];export{r as Default,c as __namedExportsOrder,d as default};
