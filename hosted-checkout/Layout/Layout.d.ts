16187724e55e5e2d5f6ed5f8d1e4547eba0d2682
yesaf <yesaf@users.noreply.github.com>
2025-03-14 12:52:39 +0000

    feat: hc branding container (#88)

--- hosted-checkout/Layout/Layout.d.ts
+++ hosted-checkout/Layout/Layout.d.ts
@@ -6,4 +6,5 @@ export declare const HCBorderBox: ({ children, className, }: PropsWithChildren<{
     className?: string;
 }>) => import("react/jsx-runtime").JSX.Element;
 export declare const HCBorderBoxHeader: ({ children }: PropsWithChildren) => import("react/jsx-runtime").JSX.Element;
+export declare const HCBrandingBox: ({ children }: PropsWithChildren) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=Layout.d.ts.map
