c9cf71729da216d6cdfa9bcdfce827a55b143e9e
hanipcode <hanipcode@users.noreply.github.com>
2025-03-19 14:26:16 +0000

    fix: wrong props for link props (#112)

--- components/Table/TableCellLinkProvider.d.ts
+++ components/Table/TableCellLinkProvider.d.ts
@@ -3,9 +3,9 @@ import { ElementType, ReactNode } from '../../../node_modules/react';
 interface TableCellLinkProviderProps {
     href: string;
     hrefAs?: ElementType;
-    hrefProps?: Record<string, unknown>;
+    linkProps?: Record<string, unknown>;
     children: ReactNode;
 }
-export declare const TableCellLinkProvider: ({ href, hrefAs, hrefProps, children, }: TableCellLinkProviderProps) => import("react/jsx-runtime").JSX.Element;
+export declare const TableCellLinkProvider: ({ href, hrefAs, linkProps, children, }: TableCellLinkProviderProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=TableCellLinkProvider.d.ts.map
