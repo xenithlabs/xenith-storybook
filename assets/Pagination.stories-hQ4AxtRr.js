439ca42d20be8d127290e886b501b28170651df2
yesaf <yesaf@users.noreply.github.com>
2025-03-24 10:03:41 +0000

    chore(main): release 0.22.4 (#123)

---
+++ assets/Pagination.stories-hQ4AxtRr.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{P as r,a as c,b as d,c as l,d as x}from"./Pagination-CCeiR1gC.js";import"./index-DRjF_FHU.js";import"./Icon-Bs9G1Rwk.js";import"./cn-C-5SdYIA.js";import"./index-Bb4qSo10.js";import"./chevron-left-rLDlePxE.js";import"./chevron-right-BqZwpdvf.js";import"./Button-X8fnom3X.js";import"./index-HZDBD2ik.js";import"./styles-BNEhTmYy.js";import"./spinner-fxOeMmbK.js";import"./chevron-down-DqISSPB-.js";import"./useClickOutside-0HkMd2oG.js";import"./search-CWRmq9WO.js";import"./close-CAN3cq4E.js";import"./stringCase-DaZl3p-i.js";import"./check-rKbD1QKS.js";import"./index-DuXoWBk8.js";import"./index-CxRLcT2J.js";import"./floating-ui.dom-BuxmzwbT.js";import"./index-CAHntWCK.js";import"./Heading-BbwmavmP.js";import"./Checkbox-C6NF-Wyv.js";import"./index-Bu1UD4WD.js";import"./index-KY2xTor3.js";import"./index-BhXpUKZt.js";const F={title:"Xenith UI/Components/Pagination",component:r,tags:["autodocs"],decorators:g=>e.jsx("div",{className:"h-[300px]",children:e.jsx(g,{})}),render:()=>e.jsxs(r,{total:113,children:[e.jsx(c,{}),e.jsxs("div",{className:"flex gap-2 items-center",children:[e.jsx(d,{}),e.jsxs("div",{className:"flex gap-1",children:[e.jsx(l,{}),e.jsx(x,{})]})]})]})},i={},t={render:()=>e.jsxs(r,{pageSize:50,children:[e.jsx(c,{}),e.jsxs("div",{className:"flex gap-2 items-center",children:[e.jsx(d,{}),e.jsxs("div",{className:"flex gap-1",children:[e.jsx(l,{}),e.jsx(x,{})]})]})]})};var a,s,o;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(o=(s=i.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var n,m,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => <Pagination pageSize={50}>
+      <PaginationSizeSetter />
+      <div className="flex gap-2 items-center">
+        <PaginationTotal />
+        <div className="flex gap-1">
+          <PaginationPrev />
+          <PaginationNext />
+        </div>
+      </div>
+    </Pagination>
+}`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const G=["Default","WithPredefinedProps"];export{i as Default,t as WithPredefinedProps,G as __namedExportsOrder,F as default};
