553a3c5f5ac72b8cf0312c87ee839374ef4bd476
yesaf <yesaf@users.noreply.github.com>
2025-04-04 07:29:42 +0000

    fix: try fix map icon export (#131)

--- components/Map/Map.d.ts
+++ components/Map/Map.d.ts
@@ -1,9 +1,9 @@
 import { PropsWithChildren } from '../../../node_modules/react';
-import { Icon as MapIcon } from 'leaflet';
+import { default as L } from 'leaflet';
 import { MapContainerProps } from 'react-leaflet';
 
 export declare const Map: ({ children, ...props }: PropsWithChildren<MapContainerProps>) => import("react/jsx-runtime").JSX.Element;
-export declare const MapMarker: import('../../../node_modules/react').ForwardRefExoticComponent<import('react-leaflet').MarkerProps & import('../../../node_modules/react').RefAttributes<import('leaflet').Marker<any>>>;
-export declare const MapMarkerPopup: import('../../../node_modules/react').ForwardRefExoticComponent<import('react-leaflet').PopupProps & import('../../../node_modules/react').RefAttributes<import('leaflet').Popup>>;
-export { MapIcon };
+export declare const MapMarker: import('../../../node_modules/react').ForwardRefExoticComponent<import('react-leaflet').MarkerProps & import('../../../node_modules/react').RefAttributes<L.Marker<any>>>;
+export declare const MapMarkerPopup: import('../../../node_modules/react').ForwardRefExoticComponent<import('react-leaflet').PopupProps & import('../../../node_modules/react').RefAttributes<L.Popup>>;
+export declare const MapIcon: typeof L.Icon;
 //# sourceMappingURL=Map.d.ts.map
