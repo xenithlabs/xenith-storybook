c85130094505ba17a4e6d3ce6de6a75ec880061f
hanipcode <hanipcode@users.noreply.github.com>
2025-04-14 10:51:10 +0000

    feat: update filter onParamsChanged (#153)

--- components/Filter/FilterPrimitive.d.ts
+++ components/Filter/FilterPrimitive.d.ts
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from '../../../node_modules/react';
 import { FilterType } from './interfaces/filter';
 
-interface FilterProps extends PropsWithChildren {
+interface FilterProps<T = unknown> extends PropsWithChildren {
     onApply: (filter: FilterType) => void;
     open?: boolean;
     onOpenChange?: (opened: boolean) => void;
+    searchParams?: T;
+    onSearchParamsChanged?: (filter: FilterType, updatedParams: T) => {};
 }
-declare const FilterProvider: ({ children, onApply }: FilterProps) => import("react/jsx-runtime").JSX.Element;
+declare const FilterProvider: ({ children, onApply, searchParams, onSearchParamsChanged, }: FilterProps) => import("react/jsx-runtime").JSX.Element;
 export { FilterProvider };
 //# sourceMappingURL=FilterPrimitive.d.ts.map
