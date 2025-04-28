4c877c7e2a30324388668f08a2d4c3f8d88be484
yesaf <yesaf@users.noreply.github.com>
2025-04-28 08:00:46 +0000

    fix: swap pay in and out icons (#178)
    * fix: swap pay in and out icons
    
    [XEN-2096]
    
    * feat: text area raw placeholder

---
+++ assets/Truncate.stories-DhM_CfDf.js
@@ -0,0 +1,142 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{T as l}from"./Truncate-DqJvsXlh.js";import{r as s}from"./index-DRjF_FHU.js";import{T as p,a as x,b as o,c as a,d as g,e as n}from"./Table-m5T3ruB0.js";import{C as c}from"./CopyButton-CRNYhgdN.js";import{B as C}from"./Badge-BZQBR-Yn.js";import"./cn-C-5SdYIA.js";import"./Tooltip-ByR2nsEC.js";import"./index-DuXoWBk8.js";import"./index-CxRLcT2J.js";import"./index-DdILnG7L.js";import"./index-qtGI3euC.js";import"./index-CxZrjbHI.js";import"./floating-ui.dom-BuxmzwbT.js";import"./index-KY2xTor3.js";import"./index-BhXpUKZt.js";import"./index-BXQDTo2X.js";import"./Icon-Bs9G1Rwk.js";import"./index-Bb4qSo10.js";import"./reset-B7P2r9As.js";import"./styles-BNEhTmYy.js";import"./Text-BYjIVLkz.js";import"./index-HZDBD2ik.js";import"./Button-X8fnom3X.js";import"./spinner-fxOeMmbK.js";import"./chevron-down-DqISSPB-.js";import"./check-rKbD1QKS.js";import"./copy-BA7Vn8GV.js";const be={title:"Xenith UI/Components/Truncate",component:l,tags:["autodocs"],args:{width:80,fluid:!1,children:"Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum super long text Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum super long text"}},i={},d={args:{withTooltip:!0}},m={args:{withTooltip:!0,withCopy:!0}},u={args:{withTooltip:!0,withCopy:!0,middleTruncate:!0}},T={args:{width:40,fluid:!0},render:r=>{const t=s.useRef(null);return s.useEffect(()=>{console.log(t)},[t]),e.jsxs(p,{children:[e.jsx(x,{children:e.jsxs(o,{children:[e.jsx(a,{children:"Name"}),e.jsx(a,{children:"Currency"}),e.jsx(a,{children:"Payment Method"}),e.jsx(a,{className:"w-[160px]",children:"Payment Channel"}),e.jsx(a,{align:"center",className:"w-[160px]",children:"Status"})]})}),e.jsx(g,{children:e.jsxs(o,{children:[e.jsx(n,{action:e.jsx(c,{text:"ayam goreng"}),children:e.jsx(l,{...r})}),e.jsx(n,{action:e.jsx(c,{text:"ayam goreng"}),children:e.jsx(l,{...r})}),e.jsx(n,{children:"Virtual Accounts"}),e.jsx(n,{children:"BCA"}),e.jsx(n,{align:"center",children:e.jsx(C,{theme:"success",variant:"soft",children:"Active"})})]})})]})}},h={args:{width:40,fluid:!0},render:r=>{const t=s.useRef(null);return s.useEffect(()=>{console.log(t)},[t]),e.jsxs(p,{children:[e.jsx(x,{children:e.jsxs(o,{children:[e.jsx(a,{children:"Name"}),e.jsx(a,{children:"Currency"}),e.jsx(a,{children:"Payment Method"}),e.jsx(a,{className:"w-[160px]",children:"Payment Channel"}),e.jsx(a,{align:"center",className:"w-[160px]",children:"Status"})]})}),e.jsx(g,{children:e.jsxs(o,{children:[e.jsx(n,{action:e.jsx(c,{text:"ayam goreng"}),children:e.jsx(l,{...r,children:"ayam goreng"})}),e.jsx(n,{action:e.jsx(c,{text:"ayam goreng"}),children:e.jsx(l,{...r,children:"ayam goreng"})}),e.jsx(n,{children:"Virtual Accounts"}),e.jsx(n,{children:"BCA"}),e.jsx(n,{align:"center",children:e.jsx(C,{theme:"success",variant:"soft",children:"Active"})})]})})]})}},b={args:{width:40,fluid:!0},render:r=>{const t=s.useRef(null);return s.useEffect(()=>{console.log(t)},[t]),e.jsxs(p,{children:[e.jsx(x,{children:e.jsxs(o,{children:[e.jsx(a,{children:"Name"}),e.jsx(a,{children:"Currency"}),e.jsx(a,{children:"Payment Method"}),e.jsx(a,{className:"w-[160px]",children:"Payment Channel"}),e.jsx(a,{align:"center",className:"w-[160px]",children:"Status"})]})}),e.jsx(g,{children:e.jsxs(o,{children:[e.jsx(n,{action:e.jsx(c,{text:"ayam goreng"}),children:e.jsx(l,{maxWidth:"fit-content",...r,children:"ayam goreng"})}),e.jsx(n,{action:e.jsx(c,{text:"ayam goreng"}),children:e.jsx(l,{maxWidth:"fit-content",...r,children:"ayam goreng"})}),e.jsx(n,{children:"Virtual Accounts"}),e.jsx(n,{children:"BCA"}),e.jsx(n,{align:"center",children:e.jsx(C,{theme:"success",variant:"soft",children:"Active"})})]})})]})}};var j,y,f;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:"{}",...(f=(y=i.parameters)==null?void 0:y.docs)==null?void 0:f.source}}};var w,H,R;d.parameters={...d.parameters,docs:{...(w=d.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    withTooltip: true
+  }
+}`,...(R=(H=d.parameters)==null?void 0:H.docs)==null?void 0:R.source}}};var B,A,N;m.parameters={...m.parameters,docs:{...(B=m.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    withTooltip: true,
+    withCopy: true
+  }
+}`,...(N=(A=m.parameters)==null?void 0:A.docs)==null?void 0:N.source}}};var W,v,M;u.parameters={...u.parameters,docs:{...(W=u.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    withTooltip: true,
+    withCopy: true,
+    middleTruncate: true
+  }
+}`,...(M=(v=u.parameters)==null?void 0:v.docs)==null?void 0:M.source}}};var S,E,P;T.parameters={...T.parameters,docs:{...(S=T.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    width: 40,
+    fluid: true
+  },
+  render: args => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      console.log(containerRef);
+    }, [containerRef]);
+    return <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Currency</TableHead>
+            <TableHead>Payment Method</TableHead>
+            <TableHead className="w-[160px]">Payment Channel</TableHead>
+            <TableHead align="center" className="w-[160px]">
+              Status
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell action={<CopyButton text="ayam goreng" />}>
+              <Truncate {...args} />
+            </TableCell>
+
+            <TableCell action={<CopyButton text="ayam goreng" />}>
+              <Truncate {...args} />
+            </TableCell>
+            <TableCell>Virtual Accounts</TableCell>
+            <TableCell>BCA</TableCell>
+            <TableCell align="center">
+              <Badge theme="success" variant="soft">
+                Active
+              </Badge>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>;
+  }
+}`,...(P=(E=T.parameters)==null?void 0:E.docs)==null?void 0:P.source}}};var D,V,L;h.parameters={...h.parameters,docs:{...(D=h.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    width: 40,
+    fluid: true
+  },
+  render: args => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      console.log(containerRef);
+    }, [containerRef]);
+    return <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Currency</TableHead>
+            <TableHead>Payment Method</TableHead>
+            <TableHead className="w-[160px]">Payment Channel</TableHead>
+            <TableHead align="center" className="w-[160px]">
+              Status
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell action={<CopyButton text="ayam goreng" />}>
+              <Truncate {...args} children="ayam goreng" />
+            </TableCell>
+
+            <TableCell action={<CopyButton text="ayam goreng" />}>
+              <Truncate {...args} children="ayam goreng" />
+            </TableCell>
+            <TableCell>Virtual Accounts</TableCell>
+            <TableCell>BCA</TableCell>
+            <TableCell align="center">
+              <Badge theme="success" variant="soft">
+                Active
+              </Badge>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>;
+  }
+}`,...(L=(V=h.parameters)==null?void 0:V.docs)==null?void 0:L.source}}};var _,I,O;b.parameters={...b.parameters,docs:{...(_=b.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    width: 40,
+    fluid: true
+  },
+  render: args => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      console.log(containerRef);
+    }, [containerRef]);
+    return <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Currency</TableHead>
+            <TableHead>Payment Method</TableHead>
+            <TableHead className="w-[160px]">Payment Channel</TableHead>
+            <TableHead align="center" className="w-[160px]">
+              Status
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell action={<CopyButton text="ayam goreng" />}>
+              <Truncate maxWidth="fit-content" {...args} children="ayam goreng" />
+            </TableCell>
+
+            <TableCell action={<CopyButton text="ayam goreng" />}>
+              <Truncate maxWidth="fit-content" {...args} children="ayam goreng" />
+            </TableCell>
+            <TableCell>Virtual Accounts</TableCell>
+            <TableCell>BCA</TableCell>
+            <TableCell align="center">
+              <Badge theme="success" variant="soft">
+                Active
+              </Badge>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>;
+  }
+}`,...(O=(I=b.parameters)==null?void 0:I.docs)==null?void 0:O.source}}};const pe=["Default","WithTooltip","WithCopy","WithMiddleTruncate","TruncateTableDynamic","TruncateTableWithoutMaxWidth","TruncateTableWithMaxWidth"];export{i as Default,T as TruncateTableDynamic,b as TruncateTableWithMaxWidth,h as TruncateTableWithoutMaxWidth,m as WithCopy,u as WithMiddleTruncate,d as WithTooltip,pe as __namedExportsOrder,be as default};
--- components/TextArea/TextArea.d.ts
+++ components/TextArea/TextArea.d.ts
@@ -1,6 +1,7 @@
 import * as React from "react";
 export interface TextAreaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
     containerClassName?: string;
+    rawPlaceholder?: string;
     error?: boolean;
 }
 declare const TextArea: React.ForwardRefExoticComponent<TextAreaProps & React.RefAttributes<HTMLTextAreaElement>>;
