dbc135a217261e39413ed6b4d31795d0eb693602
hanipcode <hanipcode@users.noreply.github.com>
2025-05-05 08:12:55 +0000

    feat: date time picker utc (#186)
    * feat: date time picker utc
    
    * feat: date time picker utc
    
    * feat: date time picker use effect utc
    
    * chore: remove unintended file
    
    * chore: remove unintended file
    
    * fix: lint fixes
    
    * feat: add extensibility in date range picker
    
    * feat: add extensibility in date range picker
    
    * fix: lint fixes

--- components/DateRangePicker/DateRangePicker.d.ts
+++ components/DateRangePicker/DateRangePicker.d.ts
@@ -1,5 +1,6 @@
 import { DateRange, PropsRange, PropsRangeRequired } from 'react-day-picker';
 import { RANGE_TEMPLATES } from './templates';
+import { DateTimePicker } from '../DateTimePicker';
 
 export type DateRangeType = DateRange;
 type DateRangePickerProps = Omit<PropsRange | PropsRangeRequired, "selected" | "onSelect" | "mode"> & {
@@ -11,9 +12,10 @@ type DateRangePickerProps = Omit<PropsRange | PropsRangeRequired, "selected" | "
     fromDate?: Date;
     rangeTemplates?: typeof RANGE_TEMPLATES;
     toDate?: Date;
+    timePickerProps?: React.ComponentPropsWithoutRef<typeof DateTimePicker>;
     error?: boolean;
     dateFormat?: string | [string, string];
 };
-export declare const DateRangePicker: ({ value, onChange, required, fromDate, toDate, rangeTemplates: rangeTemplatesProps, placeholder, error, dateFormat, showTemplateInValue, }: DateRangePickerProps) => import("react/jsx-runtime").JSX.Element;
+export declare const DateRangePicker: ({ value, onChange, required, fromDate, toDate, rangeTemplates: rangeTemplatesProps, placeholder, timePickerProps, error, dateFormat, showTemplateInValue, }: DateRangePickerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=DateRangePicker.d.ts.map
--- components/DateTimePicker/DateTimePicker.d.ts
+++ components/DateTimePicker/DateTimePicker.d.ts
@@ -1,8 +1,11 @@
+import { DateArg } from 'date-fns';
 import { PopoverContent } from '../Popover';
 import * as React from "react";
 interface DateTimePickerProps extends Omit<React.ComponentPropsWithoutRef<"input">, "onChange"> {
     date: Date | undefined;
     onChange: (date?: Date) => void;
+    dateTransformer?: (date?: Date) => Date | undefined;
+    formatFn?: (date: DateArg<Date> | string, format: string) => string;
     fromDate?: Date;
     toDate?: Date;
     error?: boolean;
@@ -11,6 +14,6 @@ interface DateTimePickerProps extends Omit<React.ComponentPropsWithoutRef<"input
     popoverProps?: React.ComponentProps<typeof PopoverContent>;
     withoutSaveButton?: boolean;
 }
-export declare const DateTimePicker: ({ date: dateProps, fromDate, toDate, onChange, error, timeError, timeMessage, withoutSaveButton, popoverProps, ...inputProps }: DateTimePickerProps) => import("react/jsx-runtime").JSX.Element;
+export declare const DateTimePicker: ({ date: dateProps, fromDate, toDate, onChange, error, timeError, dateTransformer, timeMessage, withoutSaveButton, popoverProps, formatFn, ...inputProps }: DateTimePickerProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=DateTimePicker.d.ts.map
