3cf69bb642c11d6e927499148a990485822c10b2
yesaf <yesaf@users.noreply.github.com>
2025-05-14 10:13:15 +0000

    feat: info tooltip (#210)

---
+++ components/Tooltip/InfoTooltip.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react" />
+interface InformationTooltipProps {
+    content: string | React.ReactNode;
+    triggerClassName?: string;
+    contentClassName?: string;
+}
+export declare const InfoTooltip: ({ content, triggerClassName, contentClassName, }: InformationTooltipProps) => import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=InfoTooltip.d.ts.map
