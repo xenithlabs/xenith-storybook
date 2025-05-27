ff428f7aed9fd0ea6865194ae64be696e98d29ff
yesaf <yesaf@users.noreply.github.com>
2025-05-27 15:24:57 +0000

    fix: add sidebar context export (#252)

--- components/Sidebar/index.d.ts
+++ components/Sidebar/index.d.ts
@@ -3,6 +3,6 @@ export { SidebarItem } from './SidebarItem';
 export { SidebarSubItem } from './SidebarSubItem';
 export { SidebarGroupTitle } from './SidebarGroupTitle';
 export { SidebarButton } from './SidebarButton';
-export { SidebarProvider, type SidebarContextType } from './SidebarContext';
+export { SidebarProvider, SidebarContext } from './SidebarContext';
 export { useSidebar } from './useSidebar';
 //# sourceMappingURL=index.d.ts.map
