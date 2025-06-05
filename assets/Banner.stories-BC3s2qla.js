1962aa2b50dcca58be746a99eff82b4947ade606
hanipcode <hanipcode@users.noreply.github.com>
2025-06-05 08:35:38 +0000

    feat: sidebar button margin left (#332)

---
+++ assets/Banner.stories-BC3s2qla.js
@@ -0,0 +1,12 @@
+import{j as t}from"./jsx-runtime-DR9Q75dM.js";import{B as p}from"./Banner-BmHr2JjK.js";import{F as l}from"./close-CAN3cq4E.js";import{B as s}from"./Button-DMWgXEgk.js";import{I as d}from"./Icon-CTnhpE1d.js";import"./index-DRjF_FHU.js";import"./index-Bb4qSo10.js";import"./cn-8RyR4BqC.js";import"./info-eaZ-klUF.js";import"./index-HZDBD2ik.js";import"./styles-x1yxmyQd.js";import"./chevron-up-BwzgbyHO.js";import"./chevron-down-DMq2BRSp.js";import"./spinner-jGAO5kda.js";const O={title:"Xenith UI/Components/Banner",component:p,tags:["autodocs"],args:{variant:"simple",theme:"neutral",children:"Banner Title"},argTypes:{variant:{options:["simple"],control:{type:"select"}},theme:{options:["neutral","accent","warning","success","destructive"],control:{type:"select"}}}},e={},n={args:{rightComponent:t.jsxs("div",{className:"flex items-center gap-4 font-semibold text-inherit [&>*]:h-8",children:[t.jsx(s,{variant:"unstyled",className:"px-[15.5px]",children:"Button"}),t.jsx(s,{variant:"unstyled",iconOnly:!0,icon:t.jsx("div",{className:"flex items-center justify-center w-5 h-5",children:t.jsx(d,{icon:t.jsx(l,{}),size:"lg"})}),className:"p-1.5"})]})}};var r,o,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(a=(o=e.parameters)==null?void 0:o.docs)==null?void 0:a.source}}};var i,c,m;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    rightComponent: <div className="flex items-center gap-4 font-semibold text-inherit [&>*]:h-8">
+        <Button variant="unstyled" className="px-[15.5px]">
+          Button
+        </Button>
+        <Button variant="unstyled" iconOnly icon={<div className="flex items-center justify-center w-5 h-5">
+              <Icon icon={<CloseIcon />} size="lg" />
+            </div>} className="p-1.5" />
+      </div>
+  }
+}`,...(m=(c=n.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const b=["Default","WithRightComponent"];export{e as Default,n as WithRightComponent,b as __namedExportsOrder,O as default};
