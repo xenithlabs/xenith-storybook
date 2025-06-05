3a49001da443ca03b3f7cb7379fc421289cee685
hanipcode <hanipcode@users.noreply.github.com>
2025-06-05 08:25:08 +0000

    feat: add tinytitle variant (#330)

--- assets/index-DpwWuem0.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./Calendar-D4vQbVz9.js";const r=t=>a=>e.tz(t,+new Date(a));export{r as t};
--- components/Text/styles.d.ts
+++ components/Text/styles.d.ts
@@ -1,4 +1,4 @@
 export declare const textStyles: (props?: ({
-    variant?: "tooltip" | "subtitle" | "semiBold" | "medium" | "regular" | "paragraph" | "badge" | "tiny" | "extraTiny" | null | undefined;
+    variant?: "tooltip" | "subtitle" | "semiBold" | "medium" | "regular" | "paragraph" | "badge" | "tiny" | "extraTiny" | "titleTiny" | null | undefined;
 } & import('class-variance-authority/types').ClassProp) | undefined) => string;
 //# sourceMappingURL=styles.d.ts.map
