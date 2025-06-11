c6de21c75a517005b556fc78a9413b5728850480
yesaf <yesaf@users.noreply.github.com>
2025-06-11 15:36:54 +0000

    chore(main): release 0.81.3 (#378)

--- assets/DarkModeToggle.stories-DtQwoSPT.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./jsx-runtime-DR9Q75dM.js";import{D as a}from"./DarkModeToggle-Qur8Ip2Z.js";import"./index-DRjF_FHU.js";import"./light-oSLeDn5p.js";import"./system-color-Dvh8u767.js";import"./ToggleGroup-DQTyiiy5.js";import"./index-CxRLcT2J.js";import"./cn-8RyR4BqC.js";import"./index-Bb4qSo10.js";import"./Icon-CTnhpE1d.js";const f={title:"Xenith UI/Components/DarkModeToggle",component:a,tags:["autodocs"]},e={render:()=>r.jsx("div",{className:"flex items-center gap-2",children:r.jsx(a,{})})};var o,t,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: () => <div className="flex items-center gap-2">
-      <DarkModeToggle />
-    </div>
-}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const D=["Default"];export{e as Default,D as __namedExportsOrder,f as default};
