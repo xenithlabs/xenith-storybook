c117a108f581f279de5dd8226844a47d5efa5d24
hanipcode <hanipcode@users.noreply.github.com>
2025-06-11 07:28:54 +0000

    feat: blur status bar (#359)

--- components/Charts/Common.d.ts
+++ components/Charts/Common.d.ts
@@ -1,5 +1,7 @@
 export declare const NoAccess: () => import("react/jsx-runtime").JSX.Element;
 export declare const Spinner: () => import("react/jsx-runtime").JSX.Element;
-export declare const LoadingChartBarPlaceholder: () => import("react/jsx-runtime").JSX.Element;
+export declare const LoadingChartBarPlaceholder: ({ isBlur, }: {
+    isBlur?: boolean;
+}) => import("react/jsx-runtime").JSX.Element;
 export declare const EmptyData: () => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=Common.d.ts.map
