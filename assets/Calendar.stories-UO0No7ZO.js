9ce98765b2f648ea773d4930bde687782a2b12c4
yesaf <yesaf@users.noreply.github.com>
2025-06-11 14:32:31 +0000

    chore(main): release 0.81.1 (#376)

---
+++ assets/Calendar.stories-UO0No7ZO.js
@@ -0,0 +1,22 @@
+import{C as f}from"./Calendar-BGfpJGzD.js";import{a as e}from"./endOfMonth-CBUZ-2Pf.js";import"./jsx-runtime-DR9Q75dM.js";import"./index-DRjF_FHU.js";import"./format-DeQ6fwDa.js";import"./differenceInCalendarDays-C3EGPyW5.js";import"./styles-x1yxmyQd.js";import"./index-Bb4qSo10.js";import"./Button-C-jt4Opy.js";import"./cn-8RyR4BqC.js";import"./index-HZDBD2ik.js";import"./chevron-up-Dp-rU9It.js";import"./chevron-down-CyV3wpL8.js";import"./spinner-ZMRwnkYw.js";import"./Icon-CTnhpE1d.js";import"./chevron-left-D0P6BdMN.js";import"./chevron-right-D4FvqfWr.js";const j={title:"Xenith UI/Components/Calendar",component:f,args:{selected:e(new Date,2),mode:"single",required:!1,onSelect:console.log}},r={},a={args:{selected:new Date}},o={args:{mode:"range",selected:{from:e(new Date,2),to:e(new Date,5)}}},t={args:{mode:"range",numberOfMonths:2,selected:{from:e(new Date,2),to:e(new Date,35)}}};var s,n,m;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var d,c,p;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    selected: new Date()
+  }
+}`,...(p=(c=a.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};var i,l,g;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    mode: "range",
+    selected: {
+      from: addDays(new Date(), 2),
+      to: addDays(new Date(), 5)
+    }
+  }
+}`,...(g=(l=o.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var u,D,w;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    mode: "range",
+    numberOfMonths: 2,
+    selected: {
+      from: addDays(new Date(), 2),
+      to: addDays(new Date(), 35)
+    }
+  }
+}`,...(w=(D=t.parameters)==null?void 0:D.docs)==null?void 0:w.source}}};const k=["Default","SelectedTodaySingle","RangeMode","WithMultipleMonthsShown"];export{r as Default,o as RangeMode,a as SelectedTodaySingle,t as WithMultipleMonthsShown,k as __namedExportsOrder,j as default};
