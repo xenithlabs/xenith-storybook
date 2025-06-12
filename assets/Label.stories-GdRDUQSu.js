396421f3d254df87f32ff085867401bae8a4113c
hanipcode <hanipcode@users.noreply.github.com>
2025-06-12 09:21:01 +0000

    fix: multi and blur (#389)

---
+++ assets/Label.stories-GdRDUQSu.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{L as m}from"./Label-CfHWTmrj.js";import{I as s}from"./useClickOutside-CiEINO1x.js";import"./index-DRjF_FHU.js";import"./index-_lyfCmBZ.js";import"./index-CxRLcT2J.js";import"./index-HZDBD2ik.js";import"./index-Bb4qSo10.js";import"./styles-x1yxmyQd.js";import"./cn-8RyR4BqC.js";import"./search-CkRkU8_d.js";import"./close-D2djoYoT.js";import"./stringCase-DaZl3p-i.js";import"./chevron-up-Dp-rU9It.js";import"./chevron-down-CyV3wpL8.js";import"./check-C0C6JGvp.js";import"./index-DuXoWBk8.js";import"./floating-ui.dom-BuxmzwbT.js";import"./Button-C-jt4Opy.js";import"./spinner-ZMRwnkYw.js";import"./Icon-CTnhpE1d.js";import"./index-CAHntWCK.js";import"./Heading-DjlBwQyS.js";import"./Checkbox-CAdLyoQE.js";import"./index-Bu1UD4WD.js";import"./index-KY2xTor3.js";import"./index-BhXpUKZt.js";const U={title:"Xenith UI/Components/Label",parameters:{layout:"centered",docs:{subtitle:"Renders an accessible label associated with controls."}},args:{required:!1},tags:["autodocs"]},r={name:"Default",args:{required:!0},render:i=>e.jsxs("div",{className:"grid gap-1",children:[e.jsx(m,{htmlFor:"Email",...i,children:"Email"}),e.jsx(s,{type:"email",placeholder:"Email"}),","]})};var t,a,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: "Default",
+  args: {
+    required: true
+  },
+  render: args => <div className="grid gap-1">
+      <Label htmlFor="Email" {...args}>
+        Email
+      </Label>
+      <Input type="email" placeholder="Email" />,
+    </div>
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const X=["DefaultLabel"];export{r as DefaultLabel,X as __namedExportsOrder,U as default};
