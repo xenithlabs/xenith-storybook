2380f46f2c7031fa552f8c0529cf9fdd372b4587
yesaf <yesaf@users.noreply.github.com>
2025-06-17 13:25:39 +0000

    fix: update timezone options (#413)

--- utils/timezone.d.ts
+++ utils/timezone.d.ts
@@ -1,8 +1,11 @@
 import { TimezoneOptionType } from '@/main';
 
-export declare const useFetchTimezoneOptions: () => {
+export declare const getTimezoneOffsetMinutes: (tz: string) => number;
+export declare const getTimezoneOffsetString: (tz: string) => string;
+export declare const useTimezoneOptions: () => {
     basicOptions: TimezoneOptionType[];
-    fetchTimezoneOptions: () => Promise<any[]>;
+    timezoneOptions: TimezoneOptionType[];
+    allTimezoneOptions: TimezoneOptionType[];
 };
 export declare const getTimezoneOptionValue: (option: unknown | TimezoneOptionType) => string;
 //# sourceMappingURL=timezone.d.ts.map
