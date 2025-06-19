a37fd3784559ccd341ff03ff84bb25af0dd4d06e
yesaf <yesaf@users.noreply.github.com>
2025-06-19 07:45:56 +0000

    fix: timezone popover open control

---
+++ assets/TimezonePopover.stories-Cr4naztA.js
@@ -0,0 +1,13 @@
+import{j as l}from"./jsx-runtime-DR9Q75dM.js";import{T as a}from"./TimezonePopover-C42kpXOp.js";import{r as d}from"./index-DRjF_FHU.js";import"./DropdownMenu-XER5NlNu.js";import"./index-DuXoWBk8.js";import"./index-CxRLcT2J.js";import"./index-CYnergnF.js";import"./index-ab-r6Yn4.js";import"./index-qtGI3euC.js";import"./index-DdILnG7L.js";import"./index-Bq6ilrdN.js";import"./index-CxZrjbHI.js";import"./floating-ui.dom-BuxmzwbT.js";import"./index-KY2xTor3.js";import"./index-BhXpUKZt.js";import"./cn-8RyR4BqC.js";import"./chevron-right-D4FvqfWr.js";import"./check-C0C6JGvp.js";import"./styles-x1yxmyQd.js";import"./index-Bb4qSo10.js";import"./clock-DRhx3Wks.js";import"./useClickOutside-DxMEJLjw.js";import"./search-CRI6LAtg.js";import"./close-D2djoYoT.js";import"./stringCase-DaZl3p-i.js";import"./chevron-up-Dp-rU9It.js";import"./chevron-down-CyV3wpL8.js";import"./mini-FlV67GHA.js";import"./Button-C-jt4Opy.js";import"./index-HZDBD2ik.js";import"./spinner-ZMRwnkYw.js";import"./Icon-CTnhpE1d.js";import"./index-CAHntWCK.js";import"./Heading-DjlBwQyS.js";import"./Checkbox-CAdLyoQE.js";import"./index-Bu1UD4WD.js";import"./Text-C8HBwI4b.js";import"./Divider-CIm89-oy.js";const Z={title:"Xenith UI/Components/TimezonePopover",component:a,tags:["autodocs"]},r={args:{className:"w-[269px]"}},o={args:{className:"w-[269px]"},render:i=>{const[c,u]=d.useState(!1);return l.jsx(a,{...i,open:c,onOpenChange:u})}};var t,e,p;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    className: "w-[269px]"
+  }
+}`,...(p=(e=r.parameters)==null?void 0:e.docs)==null?void 0:p.source}}};var m,s,n;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    className: "w-[269px]"
+  },
+  render: args => {
+    const [open, setOpen] = useState(false);
+    return <TimezonePopover {...args} open={open} onOpenChange={setOpen} />;
+  }
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const $=["Default","WithOpenControl"];export{r as Default,o as WithOpenControl,$ as __namedExportsOrder,Z as default};
