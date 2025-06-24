a738248a75d205993a07d1013a33497516645eed
yesaf <yesaf@users.noreply.github.com>
2025-06-24 08:34:31 +0000

    fix: time picker props (#428)

--- components/TimePicker/TimePicker.d.ts
+++ components/TimePicker/TimePicker.d.ts
@@ -2,13 +2,14 @@ import { ReactNode } from '../../../node_modules/react';
 
 export type TimePickerValue = `${number}:${number}:${number}`;
 export interface TimePickerProps {
-    value?: TimePickerValue;
+    value?: string | TimePickerValue;
     onChange?: (value: TimePickerValue) => void;
     title?: ReactNode;
     withoutSaveButton?: boolean;
     placeholder?: string;
     rawPlaceholder?: boolean;
     className?: string;
+    maxHours?: number;
 }
-export declare const TimePicker: ({ value: propValue, onChange, title, withoutSaveButton, placeholder, rawPlaceholder, className, }: TimePickerProps) => import("react/jsx-runtime").JSX.Element;
+export declare const TimePicker: ({ value: propValue, onChange, title, withoutSaveButton, placeholder, rawPlaceholder, className, maxHours, }: TimePickerProps) => import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=TimePicker.d.ts.map
