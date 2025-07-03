5b35ac8a51295e96521b130e84da8a7bf3751d62
yesaf <yesaf@users.noreply.github.com>
2025-07-03 10:25:24 +0000

    fix: timezone select browser timezone (#433)

--- utils/timezone.d.ts
+++ utils/timezone.d.ts
@@ -1,5 +1,6 @@
 import { TimezoneOptionType } from '@/main';
 
+export declare const BROWSER_TIMEZONE_VALUE = "LOCAL";
 export declare const getTimezoneOffsetMinutes: (tz: string) => number;
 export declare const getTimezoneOffsetString: (tz: string) => string;
 export declare const useTimezoneOptions: () => {
