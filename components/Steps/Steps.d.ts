ce549a0b94f11f103988a7156db941c8f9bcab16
yesaf <yesaf@users.noreply.github.com>
2025-07-16 07:56:12 +0000

    feat: horizontal steps (#448)

--- components/Steps/Steps.d.ts
+++ components/Steps/Steps.d.ts
@@ -3,7 +3,9 @@ interface StepsProps<StepType extends string, ActiveType extends StepType | numb
     steps: StepType[];
     onStepChange?: (step: ActiveType) => void;
     getStepLabel?: (step: StepType) => string;
+    horizontal?: boolean;
+    className?: string;
 }
-export declare const Steps: <StepType extends string, ActiveType extends number | StepType>({ active, steps, onStepChange, getStepLabel, }: StepsProps<StepType, ActiveType>) => import("react/jsx-runtime").JSX.Element;
+export declare const Steps: <StepType extends string, ActiveType extends number | StepType>({ active, steps, onStepChange, getStepLabel, horizontal, className, }: StepsProps<StepType, ActiveType>) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=Steps.d.ts.map
