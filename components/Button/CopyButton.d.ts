7244efb1d78a3c9cbd036676da16ce92a66a6859
hanipcode <hanipcode@users.noreply.github.com>
2025-07-17 05:04:24 +0000

    feat: copy all button (#455)

--- components/Button/CopyButton.d.ts
+++ components/Button/CopyButton.d.ts
@@ -1,7 +1,7 @@
 import { ButtonProps } from '@/main';
 
 interface CopyButtonProps {
-    text: string;
+    text: string | string[];
     withButtonText?: boolean;
 }
 export declare const CopyButton: ({ text, withButtonText, ...props }: ButtonProps & CopyButtonProps) => import("react/jsx-runtime").JSX.Element;
