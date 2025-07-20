a30ca9f781ae2f9c3a8d1ed5941e29e07eb737f1
yesaf <yesaf@users.noreply.github.com>
2025-07-20 17:06:14 +0000

    fix: truncate min width

--- components/Truncate/Truncate.d.ts
+++ components/Truncate/Truncate.d.ts
@@ -3,6 +3,7 @@ import { ReactNode } from '../../../node_modules/react';
 interface TruncateProps {
     children: ReactNode;
     width?: number;
+    minWidth?: number;
     maxWidth?: number | string;
     withTooltip?: boolean;
     withCopy?: boolean;
@@ -11,6 +12,6 @@ interface TruncateProps {
     middleTruncateStartLength?: number;
     middleTruncateEndLength?: number;
 }
-export declare const Truncate: ({ children, width, withTooltip, withCopy, fluid, maxWidth, middleTruncate, middleTruncateStartLength, middleTruncateEndLength, }: TruncateProps) => import("react/jsx-runtime").JSX.Element;
+export declare const Truncate: ({ children, width, withTooltip, withCopy, fluid, minWidth, maxWidth, middleTruncate, middleTruncateStartLength, middleTruncateEndLength, }: TruncateProps) => import("react/jsx-runtime").JSX.Element;
 export {};
 //# sourceMappingURL=Truncate.d.ts.map
