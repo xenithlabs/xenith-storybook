4fba0860a20d6c774573610edab4e34a287c2e18
yesaf <yesaf@users.noreply.github.com>
2025-08-21 08:36:38 +0000

    fix: currencies json version

--- constants/assetUrls.d.ts
+++ constants/assetUrls.d.ts
@@ -1,5 +1,5 @@
 export declare const ASSET_BASE_URL = "https://d3jlvns71gs4om.cloudfront.net";
-export declare const CURRENCIES_ASSET_URLS = "https://d3jlvns71gs4om.cloudfront.net/json/currencies.min.json?v=2";
+export declare const CURRENCIES_ASSET_URLS = "https://d3jlvns71gs4om.cloudfront.net/json/currencies.min.json?v=3";
 export declare const COUNTRY_ASSET_URLS = "https://d3jlvns71gs4om.cloudfront.net/json/countries.min.json";
 export declare const PHONE_CODE_ASSET_URLS = "https://d3jlvns71gs4om.cloudfront.net/json/phone.min.json";
 export declare const TIMEZONE_ASSET_URLS = "https://d3jlvns71gs4om.cloudfront.net/json/timezone.min.json";
