c926ec4b51a108ad4fed788f9ca26182b18d133b
yesaf <yesaf@users.noreply.github.com>
2025-08-25 15:57:44 +0000

    fix: currency select width 2

---
+++ assets/Form.stories-CST7jYoF.js
@@ -0,0 +1,25 @@
+import{j as r}from"./jsx-runtime-DR9Q75dM.js";import{F as t,u,a as F,b as f,c as h,d as g,e as x}from"./Form-Dj23DUEH.js";import{r as j}from"./index-DRjF_FHU.js";import{I as E,a as C,P as b}from"./useClickOutside-D5u8jvsc.js";import"./index-HZDBD2ik.js";import"./cn-8RyR4BqC.js";import"./Label-CfHWTmrj.js";import"./index-_lyfCmBZ.js";import"./index-CxRLcT2J.js";import"./index-Bb4qSo10.js";import"./styles-x1yxmyQd.js";import"./stringCase-DaZl3p-i.js";import"./close-D2djoYoT.js";import"./search-5X7N_xNn.js";import"./mini-FlV67GHA.js";import"./index-DuXoWBk8.js";import"./floating-ui.dom-BuxmzwbT.js";import"./check-C0C6JGvp.js";import"./spinner-ojW7ALd7.js";import"./chevron-down-CyV3wpL8.js";import"./clock-DRhx3Wks.js";import"./Button-DAgrt3Vy.js";import"./Icon-CTnhpE1d.js";import"./index-CAHntWCK.js";import"./Heading-DjlBwQyS.js";import"./Checkbox-CMdwPcui.js";import"./index-Bu1UD4WD.js";import"./index-KY2xTor3.js";import"./index-BhXpUKZt.js";import"./Text-C8HBwI4b.js";const rr={title:"Xenith UI/Components/Form",component:t,parameters:{docs:{description:{component:"Form component for handling user inputs."}}},args:{},render:n=>{const e=u();return j.useEffect(()=>{e.setError("username",{message:"This is an error message"})},[e]),r.jsx(t,{...n,...e,children:r.jsx(F,{control:e.control,name:"username",render:({field:m})=>r.jsxs(f,{children:[r.jsx(h,{htmlFor:"username",required:!0,children:"Username"}),r.jsx(g,{children:r.jsx(E,{placeholder:"shadcn",...m})}),r.jsx(x,{})]})})})}},o={args:{}},s={render:n=>{const e=u();return j.useEffect(()=>{e.setError("username",{message:"This is an error message"})},[e]),r.jsx("div",{className:"grid w-[180px]",children:r.jsx(t,{...n,...e,children:r.jsx(F,{control:e.control,name:"username",render:({field:m})=>r.jsxs(f,{children:[r.jsx(h,{required:!0,children:"Username"}),r.jsx(g,{children:r.jsx(E,{...m,placeholder:"Enter A number",leftComponent:r.jsx(C,{as:b,onChange:I=>console.log(I)})})}),r.jsx(x,{})]})})})})}};var a,i,p;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {}
+}`,...(p=(i=o.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var c,l,d;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: args => {
+    const form = useForm();
+    useEffect(() => {
+      form.setError("username", {
+        message: "This is an error message"
+      });
+    }, [form]);
+    return <div className="grid w-[180px]">
+        <Form {...args} {...form}>
+          <FormField control={form.control} name="username" render={({
+          field
+        }) => <FormItem>
+                <FormLabel required>Username</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Enter A number" leftComponent={<InputSelectAddon as={PhoneSelect} onChange={v => console.log(v as string)} />} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>} />
+        </Form>
+      </div>;
+  }
+}`,...(d=(l=s.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const er=["DefaultForm","PhoneInput"];export{o as DefaultForm,s as PhoneInput,er as __namedExportsOrder,rr as default};
---
+++ assets/Pagination.stories-BBR9o3e1.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{P as a,a as r,b as o,c,d as p}from"./Pagination-DcNg0BR4.js";import{r as g}from"./index-DRjF_FHU.js";import"./cn-8RyR4BqC.js";import"./chevron-left-D0P6BdMN.js";import"./chevron-right-D4FvqfWr.js";import"./Button-DAgrt3Vy.js";import"./index-HZDBD2ik.js";import"./index-Bb4qSo10.js";import"./styles-x1yxmyQd.js";import"./spinner-ojW7ALd7.js";import"./Icon-CTnhpE1d.js";import"./chevron-down-CyV3wpL8.js";import"./useClickOutside-D5u8jvsc.js";import"./stringCase-DaZl3p-i.js";import"./close-D2djoYoT.js";import"./search-5X7N_xNn.js";import"./mini-FlV67GHA.js";import"./index-DuXoWBk8.js";import"./index-CxRLcT2J.js";import"./floating-ui.dom-BuxmzwbT.js";import"./check-C0C6JGvp.js";import"./clock-DRhx3Wks.js";import"./index-CAHntWCK.js";import"./Heading-DjlBwQyS.js";import"./Checkbox-CMdwPcui.js";import"./index-Bu1UD4WD.js";import"./index-KY2xTor3.js";import"./index-BhXpUKZt.js";import"./Text-C8HBwI4b.js";const se={title:"Xenith UI/Components/Pagination",component:a,tags:["autodocs"],decorators:m=>e.jsx("div",{className:"h-[300px]",children:e.jsx(m,{})}),render:()=>e.jsxs(a,{total:113,children:[e.jsx(r,{}),e.jsxs("div",{className:"flex gap-2 items-center",children:[e.jsx(o,{}),e.jsxs("div",{className:"flex gap-1",children:[e.jsx(c,{}),e.jsx(p,{})]})]})]})},t={},s={render:()=>e.jsxs(a,{pageSize:50,children:[e.jsx(r,{}),e.jsxs("div",{className:"flex gap-2 items-center",children:[e.jsx(o,{}),e.jsxs("div",{className:"flex gap-1",children:[e.jsx(c,{}),e.jsx(p,{})]})]})]})},n={render:()=>e.jsxs(a,{pageSize:50,children:[e.jsx(r,{selectProps:{readonly:!0,checkIcon:null}}),e.jsxs("div",{className:"flex gap-2 items-center",children:[e.jsx(o,{}),e.jsxs("div",{className:"flex gap-1",children:[e.jsx(c,{}),e.jsx(p,{})]})]})]})},i={render:()=>{const[m,W]=g.useState(1),[C,y]=g.useState(50);return e.jsxs(a,{page:m,pageSize:C,onPageChange:W,onPageSizeChange:y,total:113,children:[e.jsx(r,{}),e.jsxs("div",{className:"flex gap-2 items-center",children:[e.jsx(o,{}),e.jsxs("div",{className:"flex gap-1",children:[e.jsx(c,{}),e.jsx(p,{})]})]})]})}};var l,d,x;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:"{}",...(x=(d=t.parameters)==null?void 0:d.docs)==null?void 0:x.source}}};var P,S,j;s.parameters={...s.parameters,docs:{...(P=s.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: () => <Pagination pageSize={50}>
+      <PaginationSizeSetter />
+      <div className="flex gap-2 items-center">
+        <PaginationTotal />
+        <div className="flex gap-1">
+          <PaginationPrev />
+          <PaginationNext />
+        </div>
+      </div>
+    </Pagination>
+}`,...(j=(S=s.parameters)==null?void 0:S.docs)==null?void 0:j.source}}};var u,h,v;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => <Pagination pageSize={50}>
+      <PaginationSizeSetter selectProps={{
+      readonly: true,
+      checkIcon: null
+    }} />
+      <div className="flex gap-2 items-center">
+        <PaginationTotal />
+        <div className="flex gap-1">
+          <PaginationPrev />
+          <PaginationNext />
+        </div>
+      </div>
+    </Pagination>
+}`,...(v=(h=n.parameters)==null?void 0:h.docs)==null?void 0:v.source}}};var f,N,z;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: () => {
+    const [page, setPage] = useState(1);
+    const [pageSize, setPageSize] = useState(50);
+    return <Pagination page={page} pageSize={pageSize} onPageChange={setPage} onPageSizeChange={setPageSize} total={113}>
+        <PaginationSizeSetter />
+        <div className="flex gap-2 items-center">
+          <PaginationTotal />
+          <div className="flex gap-1">
+            <PaginationPrev />
+            <PaginationNext />
+          </div>
+        </div>
+      </Pagination>;
+  }
+}`,...(z=(N=i.parameters)==null?void 0:N.docs)==null?void 0:z.source}}};const ne=["Default","WithPredefinedProps","WithReadonlySelect","WithOuterState"];export{t as Default,i as WithOuterState,s as WithPredefinedProps,n as WithReadonlySelect,ne as __namedExportsOrder,se as default};
